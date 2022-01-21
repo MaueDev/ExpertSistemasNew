c38e29e3a8cec5b4a6c1892489a23e8946949747
MaueDev <mauricio.rsc.mg@gmail.com>
2022-01-21 16:10:49 -0300

    Refatorando codigo para ReactJS Componente

--- expertsistemaspublic/src/pages/home/MaisFun.jsx
+++ expertsistemaspublic/src/pages/home/MaisFun.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import "./assets/css/MaisFun.css"
-
+function PainelAcordion(props)
+{
+    let hrefprop = "#"+props.Numb;
+    return(
+        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div className="panel panel-default">
+                <div className="panel-heading" role="tab" id="headingOne">
+                    <h4 className="panel-title">
+                        <a className="btn-accordion" role="button" data-toggle="collapse" data-parent="#accordion" href={hrefprop} aria-expanded="true" aria-controls={props.Numb}>
+                            <p className="btn-accordion" >{props.title}</p>
+                        </a>
+                    </h4>
+                </div>
+                <div id={props.Numb} className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div className="panel-body">
+                        {props.body}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 function MaisFun()
 {
     return(
@@ -11,109 +32,14 @@ function MaisFun()
                 </main>
                 <div className="Colum">
                     <div className="ColumLeft">
-                        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            <div className="panel panel-default">
-                                <div className="panel-heading" role="tab" id="headingOne">
-                                    <h4 className="panel-title">
-                                        <a className="btn-accordion" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            <p className="btn-accordion" >ACESSO DE USUÁRIOS POR NÍVEIS HIERÁRQUICOS</p>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                                    <div className="panel-body">
-                                        Maior segurança para a sua empresa. Tenha o controle de quais funções estarão disponíveis para os funcionários qualificados e quais usuários terão acesso administrativo total.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            <div className="panel panel-default">
-                                <div className="panel-heading" role="tab" id="headingTwo">
-                                    <h4 className="panel-title">
-                                        <a className="btn-accordion" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                            <p className="btn-accordion" >CADASTRO DE PRODUTOS AVANÇADO</p>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                                    <div className="panel-body">
-                                        Cadastre todas as variações de produtos, facilite notavelmente sua organização de produtos e estoques para e-commerce, exporte relatórios detalhados
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            <div className="panel panel-default">
-                                <div className="panel-heading" role="tab" id="headingTwo">
-                                    <h4 className="panel-title">
-                                        <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                            <p className="btn-accordion" >CADASTRO DE CLIENTES</p>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                                    <div className="panel-body">
-                                    Crie novas possibilidades, como promoções e descontos. Organize seus clientes recorrentes e disponibilize diversas formas de pagamento para diversos tipos de negociações:
-                                    Varejo, atacado e B2B
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                        <PainelAcordion Numb="collapseOne" title="ACESSO DE USUÁRIOS POR NÍVEIS HIERÁRQUICOS" body="Maior segurança para a sua empresa. Tenha o controle de quais funções estarão disponíveis para os funcionários qualificados e quais usuários terão acesso administrativo total."/>
+                        <PainelAcordion Numb="collapseTwo" title="CADASTRO DE PRODUTOS AVANÇADO" body="Cadastre todas as variações de produtos, facilite notavelmente sua organização de produtos e estoques para e-commerce, exporte relatórios detalhados."/>
+                        <PainelAcordion Numb="collapseTwo" title="CADASTRO DE CLIENTES" body="Crie novas possibilidades, como promoções e descontos. Organize seus clientes recorrentes e disponibilize diversas formas de pagamento para diversos tipos de negociações:Varejo, atacado e B2B."/>
                     </div>
                     <div className="ColumRight">
-                        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            <div className="panel panel-default" >
-                                <div className="panel-heading" role="tab" id="headingOne1">
-                                    <h4 className="panel-title">
-                                        <a className="btn-accordion"  role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne1" aria-expanded="true" aria-controls="collapseOne1">
-                                            <p className="btn-accordion"  >CONTAS A PAGAR E RECEBER</p>
-                                        </a>
-                                    </h4>
-                                </div>
-                            <div id="collapseOne1" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne1">
-                                <div className="panel-body">
-                                    Organize e produza relatórios precisos do fluxo de caixa da sua empresa. Controle gastos e investimentos de forma eficiente. Ter uma visão ampla das finanças é um passo importante para um crescimento sustentável.														 </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            <div className="panel panel-default">
-                                <div className="panel-heading" role="tab" id="headingTwo1">
-                                    <h4 className="panel-title">
-                                        <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo1" aria-expanded="false" aria-controls="collapseTwo1">
-                                            <p className="btn-accordion" >DIVERSIDADE DE PAGAMENTOS</p>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseTwo1" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo1">
-                                    <div className="panel-body">
-                                    Escolha entre carnês, boletos, cartão de crédito, cheque, dinheiro e débito. Mais diversidade de escolhas para seus clientes.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                            <div class="panel panel-default">
-                                <div class="panel-heading" role="tab" id="headingThree1">
-                                    <h4 class="panel-title">
-                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree1" aria-expanded="false" aria-controls="collapseThree1">
-                                            <p class="btn-accordion">SISTEMA INTEGRADO</p>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseThree1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree1">
-                                    <div class="panel-body">
-                                        Tenha um ERP completo em um só programa. Não dependa mais de softwares de terceiros para sua organização empresarial
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <PainelAcordion Numb="collapseOne1" title="CONTAS A PAGAR E RECEBER" body="Organize e produza relatórios precisos do fluxo de caixa da sua empresa. Controle gastos e investimentos de forma eficiente. Ter uma visão ampla das finanças é um passo importante para um crescimento sustentável."/>
+                        <PainelAcordion Numb="collapseTwo1" title="DIVERSIDADE DE PAGAMENTOS" body="Escolha entre carnês, boletos, cartão de crédito, cheque, dinheiro e débito. Mais diversidade de escolhas para seus clientes."/>
+                        <PainelAcordion Numb="collapseThree1" title="SISTEMA INTEGRADO" body="Tenha um ERP completo em um só programa. Não dependa mais de softwares de terceiros para sua organização empresarial."/>
                     </div>
                 </div>
             </div>
